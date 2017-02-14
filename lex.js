58c09cde1d46cf5bfb0570eaf0951c997b00e7e8
colby-moore <colbym2014@gmail.com>
2017-02-14 18:40:02 -0500

    uhhh deleted and restarted

--- lex.js
+++ lex.js
@@ -1,54 +1,48 @@
 
-function tokenizer(input){
-    var current = 0;
-    var token = [];
+  var T_LBracket = /[{]/;
+  var T_RBracket = /[}]/;
+  var T_LParen = /[(]/;
+  var T_RParen = /[)]/;
+  var T_Plus  = /[+]/;
+  var T_Equals  = /[==]/;
+  var T_Notequal = /[!=]/;
+  var T_Assign = /[=]/;
+  var T_Quote = /["]/;
+  var T_True = /[true]/;
+  var T_False = /[false]/;
+  var T_Int = /[int]/;
+  var T_String = /[string]/;
+  var T_While = /[while]/;
+  var T_If  = /[if]/;
+  var T_Print = /[print]/;
+  var T_Digit = /[0-9]/;
+  var T_Char = /[a-z]/;
+  var T_Newline = /[\n]/;
+  var T_Eol = /[$]/;
 
-    while( current < input.length){
-        var char = input[current];
-    }
-
-    {
-      var T_LBracket = /[{]/;
-      var T_RBracket = /[}]/;
-      var T_LParen = /[(]/;
-      var T_RParen = /[)]/;
-      var T_Plus  = /[+]/;
-      var T_Equals  = /[==]/;
-      var T_Notequal = /[!=]/;
-      var T_Assign = /[=]/;
-      var T_Quote = /["]/;
-      var T_True = /[true]/;
-      var T_False = /[false]/;
-      var T_Int = /[int]/;
-      var T_String = /[string]/;
-      var T_While = /[while]/;
-      var T_If  = /[if]/;
-      var T_Print = /[print]/;
-      var T_Digit = /[0-9]/;
-      var T_Char = /[a-z]/;
-      var T_Eol = /[$]/;
-      var {/*this is whitespace*/} =  /[whitespace]/;
+var newToken = class {
+    constructor(desc, type, line_number){
+      this.desc = desc;
+      this.type = type;
+      this.line_number = line_number;
     }
 }
 
-function LBracket (case 0){
-    if input[current] = /{/
-      return token(T_LBracket);
-}
+function lexer() {
+    var input_text = document.getElementById('input').value;
+    var forward = 0;
 
-function RBracket (case 1){
-    if input[current] = /}/
-      return token(T_RBracket);
-}
-function LBracket (case 0){
-    if input[current] = /{/
-      return token(T_LBracket);
-}
-function LBracket (case 0){
-    if input[current] = /{/
-      return token(T_LBracket);
+    for (lexerBegin = 0; lexerBegin<input_text.length; lexerBegin++) {
+        var c = input_text[lexerBegin];
+    }
+
 }
 
 
+
+
+
+
+  console.log(LBracket);
   console.log(5 + 6);
   console.log("hey its me");
